91a6e093580f87fe35e389ba65bca173efbca413
Olegstasiw <Olegstasiw@icloud.com>
2022-07-18 21:44:51 +0300

    Init

---
+++ Shop Manager/ContentView.swift
@@ -0,0 +1,21 @@
+//
+//  ContentView.swift
+//  Shop Manager
+//
+//  Created by mac on 18.07.2022.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        Text("Hello, world!")
+            .padding()
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
---
+++ Shop Manager/Shop_ManagerApp.swift
@@ -0,0 +1,17 @@
+//
+//  Shop_ManagerApp.swift
+//  Shop Manager
+//
+//  Created by mac on 18.07.2022.
+//
+
+import SwiftUI
+
+@main
+struct Shop_ManagerApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
